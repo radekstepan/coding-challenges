85bd4582217d8bed103f940f6109b3ec2c41ded7
Radek Stepan <dev@radekstepan.com>
2013-08-07 23:16:49 +0100

    a shorter version

--- c2/odd-word-dijkstra/solve.exs
+++ c2/odd-word-dijkstra/solve.exs
@@ -3,22 +3,19 @@ defmodule Solve do
 
     @space " "
 
-    def problem(input) do
-        loopy input, [], 0, true
-    end
+    def problem(input) do loopy input, [], 0, true end
 
-    defp loopy(input, output, length, even) do
+    defp loopy(input, output, length, is_even) do
+        bool_to_int = fn(bool) -> if bool do 1 else 0 end end
+
         case String.next_grapheme(input) do
-            { @space, tail } -> loopy tail, output ++ [ @space ], 0, !even
+            { @space, tail } -> loopy tail, output ++ [ @space ], 0, !is_even
             
             { head, tail } ->
-                cond do
-                    even ->
-                        loopy tail, output ++ [ head ], 0, true
-                    !even && length == 0 ->
-                        loopy tail, output ++ [ head ], 1, false
-                    true ->
-                        loopy tail, List.insert_at(output, - length, head), length + 1, false
+                if is_even or (!is_even and length == 0) do
+                    loopy tail, output ++ [ head ], bool_to_int.(!is_even), is_even
+                else
+                    loopy tail, List.insert_at(output, - length, head), length + 1, false
                 end
 
             :no_grapheme -> String.from_char_list! output
