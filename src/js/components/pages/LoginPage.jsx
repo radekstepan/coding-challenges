957ed5ed997cb2c06d6b0ce1d26d90ed8031c9a4
Radek Stepan <dev@radekstepan.com>
2016-03-07 19:21:38 +0100

    validate input

--- src/js/components/pages/LoginPage.jsx
+++ src/js/components/pages/LoginPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import cls from 'classnames';
 
 import Page from '../../lib/PageMixin.js';
+
+import actions from '../../actions/appActions.js';
 
 export default React.createClass({
 
@@ -8,19 +11,44 @@ export default React.createClass({
 
   mixins: [ Page ],
 
+  _onLogin() {
+    actions.emit('user.login', this.state);
+  },
+
+  _onInput(key, evt) {
+    this.setState({ [`${key}`]: evt.target.value });
+  },
+
+  getInitialState() {
+    return { 'email': null, 'password': null }
+  },
+
   render() {
+    let { app } = this.state;
+
+    console.log(app.user);
+
     return (
-      <div id="form">
+      <div id="form" className={cls({ 'loading': app.system.loading })}>
         <div className="title">login</div>
         <div className="field">
           <div className="label">email</div>
-          <input type="email" className="input" autoCorrect="off" />
+          <input
+            type="email"
+            className="input"
+            autoCorrect="off"
+            onChange={this._onInput.bind(this, 'email')}
+          />
         </div>
         <div className="field">
           <div className="label">password</div>
-          <input type="password" className="input" />
+          <input
+            type="password"
+            className="input"
+            onChange={this._onInput.bind(this, 'password')}
+          />
         </div>
-        <button className="button">
+        <button className="button" onClick={this._onLogin}>
           <span>login</span>
         </button>
       </div>
--- src/js/stores/appStore.js
+++ src/js/stores/appStore.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import emailRegex from 'email-regex';
 
 import Store from '../lib/Store.js';
 
@@ -24,8 +25,44 @@ class AppStore extends Store {
     });
   }
 
+  // A mock xhr that checks email & password on the server.
+  xhr(user, cb) {
+    setTimeout(() => {
+      if (user.password === 'password') {
+        cb('You are not authorized to view this page');
+      } else {
+        cb(null, _.extend(user, { 'id': 1 }));
+      }
+    }, 1e3);
+  }
+
   onSystemLoading(state) {
     this.set('system.loading', state);
+  }
+
+  onUserLogin(input) {
+    let done = (err, user) => {
+      if (err) {
+        this.set('user.error', err);
+      } else {
+        this.set('user', user);
+      }
+    };
+
+    // Check input.
+    for (let field in input) {
+      if (!input[field] || input[field].length < 8) {
+        return done(`The ${field} is too short`);
+      }
+    }
+
+    // Check email is email.
+    if (!emailRegex({ 'exact': true }).test(input.email)) {
+      return done('The email is not valid');
+    }
+
+    // Xhr credentials check.
+    this.xhr(input, this.cb(done));
   }
 
 }
