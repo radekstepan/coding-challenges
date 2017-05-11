f97c7d426112d2bc7c3fe905e49da27236837d6f
Radek Stepan <dev@radekstepan.com>
2017-05-11 09:01:00 -0400

    linked list partial

--- codewars/hungry-hippos/game.js
+++ codewars/hungry-hippos/game.js
@@ -2,22 +2,50 @@ function Game(board){
   this.board = board;
 }
 
+// Linked list.
+Game.prototype.link = function(a, b, l) {
+  [ a, b ] = a > b ? [ b, a ] : [ a, b ];
+  let n;
+  if (n = l[a]) {
+    while (n.n != null && n.n <= b) {
+      n = l[n.n];
+    }
+    // We are linked.
+    if (n.v && n.v === b) return true;
+    // Need to add the link.
+    l[b] = { v: b, p: n.v, n: n.n };
+    if (n.n) l[n.n].p = b;
+    if (n.p) l[n.p].n = b;
+  } else {
+    if (!(n = l[b])) {
+      n = l[b] = { v: b, p: null, n: null };
+    }
+    if (n.p) l[n.p].n = a;
+
+    l[a] = { v: a, n: b, p: n.p };
+  }
+
+  return false;
+}
+
 Game.prototype.play = function() {
-  let c = 0, g = this.board, o = {};
-  for (let i = 0; i < g.length; i++) {
-    for (let j = 0; j < g[i].length; j++) {
+  let c = 0, k = 0, g = this.board, l = {};
+  this.rows = [];
+  for (let i = 0; i < g.length; i++) { // rows
+    for (let j = 0; j < g[i].length; j++) { // columns
+      // Has food?
       if (g[i][j]) {
+        // Is the square to the north part of an area?
         const a = (i && g[i - 1][j]) ? g[i - 1][j] : 0;
+        // Is the square to the west part of an area?
         const b = (j && g[i][j - 1]) ? g[i][j - 1] : 0;
-        // Contiguous area on both ends.
+        // console.log(i, j, a, b, c);
+        // Contiguous area, potential link.
         if (i + j > 0 && a && b) {
-          // A join.
+          // A link of two areas?
           if (a !== b) {
-            // Do we know about it?
-            if (!o[a] || !o[b]) c--;
-            // Now we do...
-            o[a] = true;
-            o[b] = true;
+            // Did we know about this?
+            if (!this.link(a, b, l)) c--;
           }
           // Set as being part of this area.
           g[i][j] = a;
@@ -29,12 +57,13 @@ Game.prototype.play = function() {
           g[i][j] = b;
         // A new area was found.
         } else if (!a && !b) {
-          g[i][j] = ++c;
+          c++;
+          g[i][j] = ++k;
         }
       }
     }
+    this.rows.push(c);
   }
-
   return c;
 }
 
--- codewars/hungry-hippos/test.js
+++ codewars/hungry-hippos/test.js
@@ -1,9 +1,7 @@
 const t = require('tap');
 const Game = require('./game.js');
+const data = require('./data.js');
 
-const game = new Game([[1,0,0,0,0],
-                       [0,0,1,1,0],
-                       [1,0,1,0,1],
-                       [1,1,1,1,0],
-                       [1,1,1,0,1]]);
-t.equal(game.play(), 4);
+const game = new Game(data);
+t.equal(game.play(), 7);//10);
+// t.deepEqual(game.rows, [ 3, 5, 4, 5, 5, 6, 9, 7, 7, /*8, 7, 7, 7, 9, 11, 10*/ ]);
