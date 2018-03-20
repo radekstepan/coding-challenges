02e745b431b53123fb2982043a1675f315876018
Radek Stepan <dev@radekstepan.com>
2018-03-19 22:54:55 -0400

    toggle grid/table view

--- src/containers/ListBooks.js
+++ src/containers/ListBooks.js
@@ -1,29 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
+import cls from "classnames";
 
 import Topbar from "../components/Topbar";
 import Book from "../components/Book";
 
-const ListBooks = props => (
-  <div>
-    <Topbar />
-    <div id="main">
-      <div className="header">
-        <div className="title">Book list</div>
-        <input
-          type="button"
-          className="button"
-          value="Add a Book"
-          onClick={() => props.navigate('/add')}></input>
-      </div>
-      <div className="panel">
-        <div className="books table">
-          {props.books.map((book, idx) => <Book {...book} key={book.isbn} idx={idx + 1} />)}
+class ListBooks extends Component {
+  constructor() {
+    super();
+
+    this.state = { grid: true };
+
+    this.onToggleGrid = this.onToggleGrid.bind(this);
+  }
+
+  onToggleGrid() {
+    this.setState({ grid: !this.state.grid });
+  }
+
+  render() {
+    const {grid} = this.state;
+
+    return (
+      <div>
+        <Topbar />
+        <div id="main">
+          <div className="header">
+            <div className="title">Book list</div>
+            <div className="toggle" onClick={this.onToggleGrid}>View as a {!grid ? 'Grid' : 'Table'}</div>
+            <div className="action">
+              <input
+                type="button"
+                className="button"
+                value="Add a Book"
+                onClick={() => this.props.navigate('/add')}></input>
+            </div>
+          </div>
+          <div className="panel">
+            <div className={cls('books', grid ? 'grid' : 'table')}>
+              {this.props.books.map((book, idx) => <Book {...book} key={book.isbn} idx={idx + 1} />)}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 const mapState = state => ({
   books: state.books.list
